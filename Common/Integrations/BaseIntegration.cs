c565d6fc353e44842e0535c5ce4c4dcc425d80f4
f4iTh <34132723+f4iTh@users.noreply.github.com>
2019-04-26 19:04:01 +0300

    Update nuget packages; Add shared project

---
+++ Common/Integrations/BaseIntegration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Integrations
+{
+	internal abstract class BaseIntegration : IModIntegration
+	{
+		protected string ModID { get; }
+	}
+}
---
+++ Common/Integrations/IModIntegration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Integrations
+{
+	internal interface IModIntegration
+	{
+		string Label { get; }
+
+		bool IsLoaded { get; }
+	}
+}
