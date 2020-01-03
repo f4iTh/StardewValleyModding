31e6df77ae1f7effd1e60bc6205adfc8a63b646e
f4iTh <34132723+f4iTh@users.noreply.github.com>
2020-01-03 03:39:53 +0200

    Many changes; work-in-progress

---
+++ Common/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Common
+{
+	public static class Extensions
+	{
+		public static bool PressingShift(this KeyboardState kb)
+		{
+			return kb.IsKeyDown(Keys.LeftShift) || kb.IsKeyDown(Keys.RightShift);
+		}
+	}
+}
