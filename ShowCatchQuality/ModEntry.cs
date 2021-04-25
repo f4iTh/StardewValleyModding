e30e6cc7eab3f5d9d65a85b1d6d689a5b6903851
f4iTh <34132723+f4iTh@users.noreply.github.com>
2021-04-25 22:54:30 +0300

    Update ShowCatchQuality

--- ShowCatchQuality/ModEntry.cs
+++ ShowCatchQuality/ModEntry.cs
@@ -21,7 +21,7 @@ namespace ShowCatchQuality {
 			if ((fishQuality = this.Helper.Reflection.GetField<int>(fishingRod, "fishQuality").GetValue()) <= 0)
 				return;
 			float num = (float)(4.0 * Math.Round(Math.Sin(DateTime.UtcNow.TimeOfDay.TotalMilliseconds / 250.0), 2));
-			Rectangle? fishQualityRectangle = fishQuality == 4 ? new Rectangle?(new Rectangle(346, 392, 8, 8)) : fishQuality == 2 ? new Rectangle?(new Rectangle(346, 400, 8, 8)) : new Rectangle?(new Rectangle(338, 400, 8, 8));
+			Rectangle? fishQualityRectangle = new Rectangle?(new Rectangle(fishQuality >= 2 ? 346 : 338, fishQuality <= 2 ? 400 : 392, 8, 8));
 			e.SpriteBatch.Draw(Game1.mouseCursors, Game1.GlobalToLocal(Game1.viewport, Game1.player.Position + new Vector2(-124f, num - 284f) + new Vector2(44f, 108f)), fishQualityRectangle, Color.White, 0f, Vector2.Zero, 3f, SpriteEffects.None, 0f);
 		}
 	}
