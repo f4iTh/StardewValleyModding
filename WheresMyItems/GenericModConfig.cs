c61beb6ecea3c265c66fe0522b37de1e28da6cc3
f4iTh <34132723+f4iTh@users.noreply.github.com>
2021-04-27 04:57:53 +0300

    changes woooooooooo

---
+++ WheresMyItems/GenericModConfig.cs
@@ -0,0 +1,29 @@
+﻿using ModCommon.Integrations.GenericModConfigMenu;
+using StardewModdingAPI;
+using System;
+
+namespace WheresMyItems {
+
+	public class GenericModConfig {
+		private readonly GenericModConfigMenuIntegration<ModConfig> ConfigMenu;
+
+		public GenericModConfig(IModRegistry modRegistry, IMonitor monitor, IManifest manifest, Func<ModConfig> getConfig, Action reset, Action saveAndApply) {
+			this.ConfigMenu = new GenericModConfigMenuIntegration<ModConfig>(modRegistry, monitor, manifest, getConfig, reset, saveAndApply);
+			this.Register();
+		}
+
+		private void Register() {
+			GenericModConfigMenuIntegration<ModConfig> menu = this.ConfigMenu;
+			if (!menu.IsLoaded)
+				return;
+
+			menu.RegisterConfig(true)
+				.AddKeyBinding(
+					"Toggle Search Menu",
+					"Which button to press to open the search menu.",
+					config => config.ToggleButton,
+					(config, val) => config.ToggleButton = val
+				);
+		}
+	}
+}
---
+++ WheresMyItems/WheresMyItems.cs
@@ -0,0 +1,46 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace WheresMyItems {
+
+	public class WheresMyItems : StardewModdingAPI.Mod {
+		internal static ModIntegrations Integrations;
+
+		private ModConfig Config;
+
+		public override void Entry(IModHelper helper) {
+			this.Config = this.Helper.ReadConfig<ModConfig>();
+
+			IModEvents events = helper.Events;
+			events.GameLoop.GameLaunched += this.Initialize;
+			events.Input.ButtonsChanged += this.OnButtonsChanged;
+		}
+
+		private void Initialize(object sender, GameLaunchedEventArgs e) {
+			WheresMyItems.Integrations = new ModIntegrations(this.Helper.ModRegistry, this.Monitor);
+
+			new GenericModConfig(
+				modRegistry: this.Helper.ModRegistry,
+				monitor: this.Monitor,
+				manifest: this.ModManifest,
+				getConfig: () => this.Config,
+				reset: () => {
+					this.Config = new ModConfig();
+					this.Helper.WriteConfig(this.Config);
+					this.Helper.ReadConfig<ModConfig>();
+				},
+				saveAndApply: () => {
+					this.Helper.WriteConfig(this.Config);
+					this.Helper.ReadConfig<ModConfig>();
+				}
+			);
+		}
+
+		private void OnButtonsChanged(object sender, ButtonsChangedEventArgs e) {
+			if (!Context.IsWorldReady || Game1.player.currentLocation == null || Game1.activeClickableMenu != null || !Context.CanPlayerMove || !this.Config.ToggleButton.JustPressed())
+				return;
+			Game1.activeClickableMenu = new ModMenu(Game1.viewport.Width / 2 - (600 + global::WheresMyItems.ModMenu.borderWidth * 2) / 2, Game1.viewport.Height / 2 - (600 + global::WheresMyItems.ModMenu.borderWidth * 2) / 2, 800 + global::WheresMyItems.ModMenu.borderWidth * 2, 600 + global::WheresMyItems.ModMenu.borderWidth * 2);
+		}
+	}
+}
