244a45784107edc4d15341cb3fb9309952076422
f4iTh <34132723+f4iTh@users.noreply.github.com>
2024-02-20 14:04:38 +0200

    Major mod update;
    Translations, new config options, improved logic, ..

---
+++ ActivateSprinklers/Common/Configs/ModConfig.cs
@@ -0,0 +1,9 @@
+﻿using ActivateSprinklers.Common.Enums;
+
+namespace ActivateSprinklers.Common.Configs {
+  public class ModConfig {
+    public bool InfiniteReach { get; set; }
+    public SprinklerAnimation SprinklerAnimation { get; set; } = SprinklerAnimation.WateringCanAnimation;
+    public AdjacentTileDirection AdjacentTileDirection { get; set; } = AdjacentTileDirection.LeftRight;
+  }
+}
