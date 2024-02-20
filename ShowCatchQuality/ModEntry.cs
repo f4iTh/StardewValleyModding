78652cd489240fe1927e61ddfe778e962da7e03e
f4iTh <34132723+f4iTh@users.noreply.github.com>
2024-02-20 03:56:16 +0200

    Make code (hopefully) more readable

--- ShowCatchQuality/ModEntry.cs
+++ ShowCatchQuality/ModEntry.cs
@@ -2,42 +2,32 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Tools;
 
 namespace ShowCatchQuality {
   public class ModEntry : Mod {
     public override void Entry(IModHelper helper) {
-      helper.Events.Display.RenderedWorld += (sender, e) => {
-        if (Game1.activeClickableMenu != null ||
-            Game1.player.CurrentTool == null ||
-            !(Game1.player.CurrentTool is FishingRod fishingRod) ||
-            !((FishingRod)Game1.player.CurrentTool).fishCaught)
-          return;
-        int fishQuality;
-        if ((fishQuality = helper.Reflection.GetField<int>(fishingRod, "fishQuality").GetValue()) <= 0)
-          return;
-        float num = (float)(4.0 * Math.Round(Math.Sin(DateTime.UtcNow.TimeOfDay.TotalMilliseconds / 250.0), 2));
-        Rectangle? fishQualityRectangle = new Rectangle(
-          fishQuality >= 2 ? 346 : 338,
-          fishQuality <= 2 ? 400 : 392,
-          8,
-          8
-        );
-        e.SpriteBatch.Draw(
-          Game1.mouseCursors,
-          Game1.GlobalToLocal(
-            Game1.viewport,
-            Game1.player.Position + new Vector2(-124f, num - 284f) + new Vector2(44f, 108f)
-          ),
-          fishQualityRectangle,
-          Color.White,
-          0f,
-          Vector2.Zero,
-          3f,
-          SpriteEffects.None,
-          0f);
-      };
+      helper.Events.Display.RenderedWorld += this.OnRenderedWorld;
+    }
+
+    private void OnRenderedWorld(object sender, RenderedWorldEventArgs e) {
+      if (Game1.activeClickableMenu != null || Game1.player.CurrentTool == null || Game1.player.CurrentTool is not FishingRod fishingRod || !((FishingRod)Game1.player.CurrentTool).fishCaught)
+        return;
+
+      int fishQuality = this.Helper.Reflection.GetField<int>(fishingRod, "fishQuality").GetValue();
+      if (fishQuality <= 0)
+        return;
+
+      int fishQualityRectX = fishQuality >= 2 ? 346 : 338;
+      int fishQualityRectY = fishQuality <= 2 ? 400 : 392;
+      Rectangle? fishQualityRect = new Rectangle(fishQualityRectX, fishQualityRectY, 8, 8);
+      
+      float num = (float)(4.0 * Math.Round(Math.Sin(DateTime.UtcNow.TimeOfDay.TotalMilliseconds / 250.0), 2));
+      Vector2 position = Game1.GlobalToLocal(Game1.viewport, Game1.player.Position + new Vector2(-124f, num - 284f) + new Vector2(44f, 108f));
+      
+      e.SpriteBatch.Draw(Game1.mouseCursors, position, fishQualityRect, Color.White, 0f, Vector2.Zero, 3f, SpriteEffects.None, 0f);
     }
   }
 }
