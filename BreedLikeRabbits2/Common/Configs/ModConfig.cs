614549ea359fc3d071022a23e3eed06f42da2435
f4iTh <34132723+f4iTh@users.noreply.github.com>
2024-02-21 22:30:41 +0200

    Major update
    Better logic handling, generic mod config menu integration, default translation (i18n), ability to name newly born rabbits, ..

---
+++ BreedLikeRabbits2/Common/Configs/ModConfig.cs
@@ -0,0 +1,14 @@
+namespace BreedLikeRabbits2.Common.Configs {
+  public class ModConfig {
+    public bool NameNewRabbits { get; set; }
+    public bool IgnoreGender { get; set; }
+    // public bool CheckGendersPerCoop { get; set; } = true;
+    // public bool CheckBucksPerCoop { get; set; } = true;
+    public bool AccountForHappiness { get; set; } = true;
+    public bool AccountForFriendship { get; set; } = true;
+    public bool AccountForSeason { get; set; } = true;
+    public bool AccountForAge { get; set; } = true;
+    public bool AllowMultiples { get; set; } = true;
+    public float BaseRate { get; set; } = 42.0f;
+  }
+}
