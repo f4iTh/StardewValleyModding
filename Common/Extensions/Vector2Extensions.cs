10f0d8cc76c1df0a3803fd393d1ed2d71e6b8421
f4iTh <34132723+f4iTh@users.noreply.github.com>
2024-02-23 22:16:42 +0200

    Update common shared project

---
+++ Common/Extensions/Vector2Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace ActivateSprinklers.ModCommon.Extensions {
+  public static class Vector2Extensions {
+    public static double CalculateAngleToTarget(this Vector2 origin, Vector2 target, bool toDegrees = false) {
+      float xDiff = target.X - origin.X;
+      float yDiff = target.Y - origin.Y;
+      double angle = -Math.Atan2(yDiff, xDiff);
+      double adjustedForNegative = angle < 0 ? 2 * Math.PI + angle : angle;
+
+      return toDegrees ? adjustedForNegative * 180 / Math.PI : adjustedForNegative;
+    }
+  }
+}
