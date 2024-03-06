65b88ec4be32b3ae19a46ff8ed1aa2220aae81d4
f4iTh <34132723+f4iTh@users.noreply.github.com>
2024-03-07 01:15:36 +0200

    Rename config variable, adjust default config value

--- CustomWarps/Common/Configs/ModConfig.cs
+++ CustomWarps/Common/Configs/ModConfig.cs
@@ -6,9 +6,9 @@ namespace CustomWarps.Common.Configs {
   public class ModConfig {
     public KeybindList ToggleKey { get; set; } = new(SButton.F7);
 
-    public MenuStyle MenuStyle { get; set; } = MenuStyle.LegacyGrid;
+    public MenuStyle MenuStyle { get; set; } = MenuStyle.VerticalList;
 
     // public int MaxItemsPerGridRow { get; set; } = 8;
-    public int MaxItemsPerGridColumn { get; set; } = 3;
+    public int MaxGridColumns { get; set; } = 3;
   }
 }
