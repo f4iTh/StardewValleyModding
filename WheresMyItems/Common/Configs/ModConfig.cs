e2c5b3f0ed00e62a64240a99229a39453e6b38f9
f4iTh <34132723+f4iTh@users.noreply.github.com>
2024-03-07 01:15:43 +0200

    Adjust default config value

--- WheresMyItems/Common/Configs/ModConfig.cs
+++ WheresMyItems/Common/Configs/ModConfig.cs
@@ -6,7 +6,7 @@ namespace WheresMyItems.Common.Configs {
   public class ModConfig {
     public KeybindList ToggleButton { get; set; } = new(SButton.F5);
     public HighlightMethod ChestHighlightMethod { get; set; } = HighlightMethod.TypingRipple;
-    public bool DrawItemsOverChests { get; set; }
+    public bool DrawItemsOverChests { get; set; } = true;
     public bool LimitMaxItemsDrawnOverChests { get; set; } = true;
     public int MaxItemsDrawnOverChests { get; set; } = 3;
     public ItemDrawDirection ItemDrawDirection { get; set; } = ItemDrawDirection.Vertical;
