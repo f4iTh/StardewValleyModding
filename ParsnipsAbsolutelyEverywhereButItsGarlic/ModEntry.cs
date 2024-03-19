4442d1642ca4474801d56cbe9bef3d36da1d403f
f4iTh <34132723+f4iTh@users.noreply.github.com>
2024-03-19 21:53:35 +0200

    Update mod

--- ParsnipsAbsolutelyEverywhereButItsGarlic/ModEntry.cs
+++ ParsnipsAbsolutelyEverywhereButItsGarlic/ModEntry.cs
@@ -23,9 +23,9 @@ namespace ParsnipsAbsolutelyEverywhereButItsGarlic {
         for (int y = 0; y < farm.map.Layers[0].LayerSize.Height; y++)
           farm.makeHoeDirt(new Vector2(x, y));
 
-        foreach ((Vector2 tile, TerrainFeature terrainFeature) in farm.terrainFeatures.Pairs) {
+        foreach ((Vector2 _, TerrainFeature terrainFeature) in farm.terrainFeatures.Pairs) {
           HoeDirt dirt = terrainFeature as HoeDirt;
-          dirt?.plant(476, (int)tile.X, (int)tile.Y, Game1.player, false, farm);
+          dirt?.plant("(O)476", Game1.player, false);
           dirt?.crop?.growCompletely();
         }
       }
